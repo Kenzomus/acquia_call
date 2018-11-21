bddd827541e2801d86ffd5c7674257f8d47b3ba3
Ibrahima19076 <Ibrahima.NDiaye-cw@otsuka-us.com>
2018-11-21 13:02:19 -0500

    modify custom_http_client

--- docroot/modules/custom/custom_http_client/src/myclient_container.php
+++ docroot/modules/custom/custom_http_client/src/myclient_container.php
@@ -1,14 +1,14 @@
-</php
-**
+<?php
+   /** 
    * GuzzleHttp\Client definition.
    *
    * @var GuzzleHttp\Client
    */
   protected $custom_http_client_client;
-  public function __construct(
-      Client $custom_http_client_client
-    ) {
-    parent::__construct();
+
+  public function __construct(Client $custom_http_client_client) {
+
+        parent::__construct();
         $this->custom_http_client_client = $custom_http_client_client;
   }
 
@@ -16,4 +16,5 @@
     return new static(
       $container->get('custom_http_client.client')
     );
+  
   }
