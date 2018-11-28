3995f6dee0e9ed3b518f15a784c8da6b40e04d76
Ibrahima <kenzomu@gmail.com>
2018-11-28 17:12:45 +0000

    fix the sites report

--- docroot/modules/custom/acquia_api/src/Controller/ReportController.php
+++ docroot/modules/custom/acquia_api/src/Controller/ReportController.php
@@ -1,10 +1,17 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\acquia_api\Controller\ReportController.
  */
-namespace Drupal\acquia_api\Controller;
 
+/**
+ * Controller for the site backup and deleted List Report
+*/
+
+namespace Drupal\acquia_api\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Database;
 
@@ -18,19 +25,7 @@ class ReportController extends ControllerBase {
    *
    * @return array
    */
-  protected function load() {
-    $select = Database::getConnection()->select('acquia_api', 'r');
-      // Join the users table, so we can get the entry creator's username.
-    $select->join('users_field_data', 'u', 'r.uid = u.uid');
-      
-    $select->join()
-    // Select these specific fields for the output.
-    $select->addField('u', 'name', 'username');
-    $select->addField('n', 'site_name');
-    $select->addField('r', 'site_bkup_url');
-    $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
-    return $entries;
-  }
+
 
   /**
    * Creates the report page.
@@ -41,28 +36,30 @@ class ReportController extends ControllerBase {
   public function report() {
     $content = array();
     $content['message'] = array(
-      '#markup' => $this->t('Below is a list of all sites including username'),
+     '#markup' => $this->t('Below is a list of all sites including username'),
     );
-    $headers = array(
-      t('Name'),
-      t('site_name'),
-      t('site_bkup_url'),
-    );
-    $rows = array();
-    foreach ($entries = $this->load() as $entry) {
-      // Sanitize each entry.
-      $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', $entry);
-    }
-    $content['table'] = array(
-      '#type' => 'table',
-      '#header' => $headers,
-      '#rows' => $rows,
-      '#empty' => t('No entries available.'),
-    );
-    // Don't cache this page.
-    $content['#cache']['max-age'] = 0;
+   // $headers = array(
+     // t('Name'),
+     // t('site_name'),
+     // t('site_bkup_url'),
+   // );
+   // $rows = array();
+   // foreach ($entries = $this->load() as $entry) {
+     //  Sanitize each entry.
+     // $rows[] = array_map('Drupal\Component\Utility\SafeMarkup::checkPlain', $entry);
+   // }
+   // $content['table'] = array(
+     // '#type' => 'table',
+     // '#header' => $headers,
+     // '#rows' => $rows,
+    //  '#empty' => t('No entries available.'),
+  //  );
+   //  Don't cache this page.
+   // $content['#cache']['max-age'] = 0;
     return $content;
+
+   // return new Response($content);
   }
 
 }
-
+ 
